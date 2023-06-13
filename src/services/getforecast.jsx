dff790020fc23c42a3145399634937d6c8e78306
randit-code <randithmadhusanka@gmail.com>
2023-06-13 22:44:42 +0530

    bugfix

--- src/services/getforecast.jsx
+++ src/services/getforecast.jsx
@@ -4,7 +4,7 @@ export const getforecast = async(lat, lon) => {
     const apiKey = '64595fc2f5670b2a210b45d6f73d20ca'
     
     try{
-        const URL = `http://api.openweathermap.org/data/2.5/forecast?units=metric&lat=${lat}&lon=${lon}&appid=${apiKey}`
+        const URL = `https://api.openweathermap.org/data/2.5/forecast?units=metric&lat=${lat}&lon=${lon}&appid=${apiKey}`
         const response = await axios.get(URL);
         return response.data
     }
